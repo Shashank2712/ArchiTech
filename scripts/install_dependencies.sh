4e19c9599a0b85e8006d89832e02d05b547f208a
Shashank2712 <srk2712@gmail.com>
2024-06-21 22:47:36 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,6 +5,18 @@ source venv/bin/activate
 
 pip3 install flask
 
+# Find the PID of the Python process running the specific app.py
+PID=$(pgrep -f "python3 /home/ubuntu/pythonApp/app.py")
 
+if [ -z "$PID" ]; then
+    echo "No running Python Flask application found."
+else
+    # Terminate the Python process
+    echo "Stopping Python Flask application with PID: $PID"
+    kill $PID
+    echo "Application stopped."
+fi
 
-
+#!/bin/bash
+cd /home/ubuntu/pythonApp
+nohup python3 /home/ubuntu/pythonApp/app.py > /home/ubuntu/pythonApp/nohup.out 2>&1 &
