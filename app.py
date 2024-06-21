def67b1d6248b7317b8ff4a8025444706490c65d
Shashank2712 <srk2712@gmail.com>
2024-06-21 21:09:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is Shashank Koli's python3 app'
+    return 'Hello, Shashank's World!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
