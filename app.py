e07103ce2445c2ddced2cef32ac580b7349dfef8
Shashank2712 <srk2712@gmail.com>
2024-06-21 23:30:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Robotic World!'
 
 @app.route('/static/<path:filename>')
 def static_files(filename):
